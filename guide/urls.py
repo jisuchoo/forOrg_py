7995f8f2ec6478a2a8d74e34f090cfcda441b69d
jisuchoo <64048563+jisuchoo@users.noreply.github.com>
2025-09-06 23:59:13 +0900

    Update urls.py

--- guide/urls.py
+++ guide/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_view, name="login"),          # 로그인 페이지
-    path("search/", views.search_view, name="search"), # 검색 페이지
-    path("logout/", views.logout_view, name="logout"), # 로그아웃
+    path("", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("search/", views.search_view, name="search"),
 ]
