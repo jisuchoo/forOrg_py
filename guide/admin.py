9e65f8ba9832d07c8ae7adbf63f516db8848dcd3
jisuchoo <64048563+jisuchoo@users.noreply.github.com>
2025-09-07 22:13:12 +0900

    Update admin.py

--- guide/admin.py
+++ guide/admin.py
@@ -1,6 +1,6 @@
 # admin.py
 from django.contrib import admin
-from .models import Employee, Disease
+from .models import Employee, Disease, ActivityLog
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
@@ -9,3 +9,9 @@ class EmployeeAdmin(admin.ModelAdmin):
 @admin.register(Disease)
 class DiseaseAdmin(admin.ModelAdmin):
     list_display = ("name", "acceptance", "surgery", "recurrence")
+
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    list_display = ("user", "action", "ip_address", "created_at")
+    list_filter = ("action", "created_at")
+    search_fields = ("user__username", "detail")
