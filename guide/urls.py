7e91069766ac096ba2b2fb50d8a75802b16570fb
jisuchoo <64048563+jisuchoo@users.noreply.github.com>
2025-09-15 00:42:22 +0900

    Update urls.py

--- guide/urls.py
+++ guide/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path("", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("search/", views.search_view, name="search"),
+
+    # 상품별 인수한도 API
+    path("limits/products/", views.get_products, name="get_products"),
+    path("limits/plans/", views.get_plans, name="get_plans"),
+    path("limits/ages/", views.get_ages, name="get_ages"),
+    path("limits/results/", views.get_results, name="get_results"),
 ]
